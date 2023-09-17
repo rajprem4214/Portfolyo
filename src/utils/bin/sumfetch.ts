9923f0fd33ab51cd4f34dee07b93f6361dc46e93
Prem Raj <rajprem3230@gmail.com>
2023-09-18 04:16:27 +0530

    Update summary data

--- src/utils/bin/sumfetch.ts
+++ src/utils/bin/sumfetch.ts
@@ -8,13 +8,13 @@ const sumfetch = async (args: string[]): Promise<string> => {
         @@@@               @@@@             -----------
       @@                       @@            ABOUT
     @@                           @@          ${config.name}
-  @@                               @@       ﰩ ${config.ps1_hostname}
- @@                         @@@     @@       <u><a href="${config.resume_url}" target="_blank">resume</a></u>
-@@        @@@                        @@     爵 <u><a href="${config.repo}" target="_blank">Github repo</a></u>
+  @@                               @@       ﰩ I go by ${config.ps1_hostname} across all coding platforms.
+ @@                         @@@     @@       Former SDE Intern at Microsoft'23
+@@        @@@                        @@     爵 Competitive Programmer | Full Stack Developer
 @@                                   @@     -----------
 @@             .@@@@@@@@@@.          @@      CONTACT 
  @@           @@          @@        @@       <u><a href="mailto:${config.email}" target="_blank">${config.email}</a></u>
-  @@           @@        @@        @@        <u><a href="https://github.com/${config.social.github}" target="_blank">github.com/${config.social.github}</a></u>
+  @@           @@        @@        @@        <u><a href="https://linktr.ee/${config.ps1_hostname}" target="_blank">All coding profiles</a></u>
    @@             @@@@@@          @@         <u><a href="https://linkedin.com/in/${config.social.linkedin}" target="_blank">linkedin.com/in/${config.social.linkedin}</a></u>
      @@@                        @@@         -----------
         @@@                  @@@ @@         
